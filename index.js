6097c26a21afee7935356a65e33c0bc3d8c49b1f
E-Kirill <ezhov.kirill98@gmail.com>
2017-06-07 12:13:50 +0300

    jsdoc

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 let FB = require('./FB');
 let fb = new FB();
 
-fb.getUserName('4').then((userName)=> {
-    console.log(userName.name);
-});
+fb.getUserName('4')
+    .then((userName)=> {
+        console.log(userName.name);
+    })
+    .catch((err)=> {
+        throw Error(err);
+    });
 
-fb.getGroupById('882374948522079').then((group)=> {
-    console.log(group);
-});
+fb.getGroupById('882374948522079')
+    .then((group)=> {
+        console.log(group.name);
+    })
+    .catch((err)=> {
+        throw Error(err);
+    });
 
-fb.getGroupMembersById('882374948522079').then((members)=> {
-    console.log(members);
-});
+fb.getGroupMembersById('882374948522079')
+    .then((members)=> {
+        console.log(members.data);
+    })
+    .catch((err)=> {
+        throw Error(err);
+    });
